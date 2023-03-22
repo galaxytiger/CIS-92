302f88bcdd4947b6fde7cdb4e0cf36178fb13e3d
galaxytiger <anthony.torresmoran@gmail.com>
2023-03-22 04:52:23 +0000

    fixing error in settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': 'USER': os.environ.get('DB_USER'),
+        'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
     },
